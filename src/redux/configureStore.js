5af1555083aefc269f2c2a974c05668e7ded3cbf
Jhoam Molina <molina_504@hotmail.com>
2021-11-02 16:47:39 -0500

    React redux forms

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import { initialState, Reducer } from './reducer';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        Reducer,
+        initialState
+    );
+
+    return store;
+}
